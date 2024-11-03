6cdd97bddea5daa7e62241465758e63ae27490b4
Mustafa ULUTAŞ <128393765+mustafaulutas1@users.noreply.github.com>
2024-11-03 14:40:29 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp5/ArrayTypedStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class ArrayTypedStack
+    {
+        int MAX;
+        int top;
+        object[] stack;
+
+        public bool IsEmpty()
+        {
+            return (top < 0);
+        }
+        public ArrayTypedStack(int MAX)
+        {
+            stack = new object[MAX];
+            this.MAX = MAX;
+            top = -1;
+        }
+        public void Push(object data)
+        {
+            if (top < this.MAX)
+            {
+                stack[++top] = data;
+            }
+        }
+        public object Pop()
+        {
+            object value = new object();
+
+            if (top >= 0)
+            {
+                value = stack[top--];
+
+            }
+            return value;
+        }
+        public object Peek()
+        {
+            object value = new object();
+
+            if (top >= 0)
+                value = stack[top];
+
+            return value;
+        }
+        public void PrintStack()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Items in the Stack are :");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giriş yap:");
+            string parantez = Console.ReadLine();
+
+            if (ParantezKontrol(parantez))
+            {
+                Console.WriteLine("Doğru.");
+            }
+            else
+            {
+                Console.WriteLine("Yanlış.");
+            }
+            Console.ReadKey();
+        }
+
+        static bool ParantezKontrol(string parantez)
+        {
+            ArrayTypedStack stack = new ArrayTypedStack(parantez.Length);
+
+            foreach (char character in parantez)
+            {
+                if (character == '(' || character == '{' || character == '[')
+                {
+                    stack.Push(character);
+                }
+                else if (character == ')' || character == '}' || character == ']')
+                {
+                    if (stack.IsEmpty())
+                    {
+                        return false; 
+                    }
+
+                    char son = (char)stack.Pop();
+                    if (!Esle(son, character))
+                    {
+                        return false; 
+                    }
+                }
+            }
+
+            return stack.IsEmpty(); 
+        }
+
+        static bool Esle(char acılıs, char kapanıs)
+        {
+            return (acılıs == '(' && kapanıs == ')') ||
+                   (acılıs == '{' && kapanıs == '}') ||
+                   (acılıs == '[' && kapanıs == ']');
+        }
+    }
+}
